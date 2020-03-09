42153fbae2750fd36668a0fac51a3f65c10f4c30
zhuocongbin <13145257luodan>
2020-03-09 20:51:04 +0800

    first

--- src/main/java/com/cmiot/ota/receive/controller/ReceiverController.java
+++ src/main/java/com/cmiot/ota/receive/controller/ReceiverController.java
@@ -1,7 +1,6 @@
 package com.cmiot.ota.receive.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.DigestUtils;
